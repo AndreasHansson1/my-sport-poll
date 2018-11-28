4c463f57bb0005709dbe4e424e590c68c3fa2c89
Andreas_Hansson <32000349+AndreasHansson1@users.noreply.github.com>
2018-11-28 05:38:07 +0100

    Random win/loose

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './Result.css';
 
 const Result = () => {
+  const random = Math.random() >= 0.5;
   return (
     <div className="result-wrapper">
-      <h1>You Won!</h1>
+      {random ? <h1>You Won!</h1> : <h1>You Lost</h1>}
     </div>
   );
 };
